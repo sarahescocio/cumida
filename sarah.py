c42720b9148c10439f4fdc5e6e9a25414e943a35
sarahescocio <114763074+sarahescocio@users.noreply.github.com>
2022-09-30 22:13:31 -0300

    Meu primeirinho commit
    <3

---
+++ sarah.py
@@ -0,0 +1,12 @@
+feijao = 'bronk '
+arroz = 'brugobrugo ' + feijao
+
+print (arroz)
+
+arroz = arroz + feijao 
+print (5*arroz) 
+
+farofa = 6 
+salada = 7
+suco = farofa + salada
+print ('lu' + 'lala' + str (suco)) 
